d49366b17df64d9f959bb2786ca1015aef21d3e7
waynel <w4ynel@gmail.com>
2016-06-21 12:47:20 -0400

    setup jQuery

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,15 +1,5 @@
-window.onload = function() {
-  var checkboxes = document.getElementsByTagName('input');
-  for (i = 0; i < checkboxes.length; ++i) {
-    checkboxes[i].addEventListener('click', clickHandler);
-  }
-};
-
-function clickHandler() {
-  if(this.checked) {
-    this.nextElementSibling.className = 'checked';
-  } else {
-    this.nextElementSibling.className = '';    
-
-  }
-}
+$(function() {
+  $('input').on('click', function() {
+    $(this.nextElementSibling).toggleClass('checked');
+  });
+});
