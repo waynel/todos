53017692acc0e4b83a5c0cfd7352c71ceedb251d
waynel <w4ynel@gmail.com>
2016-06-29 01:12:02 -0400

    add form to add todo items, use jquery and ajax to make post request

--- client/script.js
+++ client/script.js
@@ -1,5 +1,35 @@
+var $ = require('jquery');
+
 $(function() {
+  $(":button").on('click', addTodo);
+  $(":text").on('keypress', function(e) {
+    var key = e.keyCode;
+    if( key == 13 || key == 169 ) {
+      addTodo();
+      e.preventDefault();
+      e.stopPropagation();
+      return false;
+    }
+  });
   $('input').on('click', function() {
     $(this.nextElementSibling).toggleClass('checked');
   });
 });
+
+var addTodo = function() {
+  var text = $('#add-todo-text').val();
+  $.ajax({
+    url: '/api/todos',
+    type: 'POST',
+    data: {
+      text: text
+    },
+    dataType: 'json',
+    success: function(data) {
+      var todo = data.todo;
+      var newLiHtml = '<li><input type="checkbox"><span> ' + todo + '</span></li>';
+      $('form + ul').append(newLiHtml);
+      $('#add-todo-text').val('');
+    }
+  });
+};
